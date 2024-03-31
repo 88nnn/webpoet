7562298b724184e98a5311a5b6055373c57211af
88nnn <130357067+88nnn@users.noreply.github.com>
2024-03-31 13:27:53 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -2,28 +2,18 @@
 # load_dotenv()
 import streamlit as st
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
 
 OPENAI_API_KEY = "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"
 
-llm = ChatOpenAI(OPENAI_API_KEY)
+llm = ChatOpenAI()
 
-"""prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are a poet."),
-    ("user", "{input}")
-])
 
-"""
-content = st.text_input("엔터로 주제어 입력")
-#output_parser = StrOutputParser()
-
-#chain = prompt | llm | output_parser
 st.title('시인')
-
+content = st.text_input("엔터로 주제어 입력")
 if st.button('시 작성'):
-    result = llm.predict(content + "에 대한 시를 써줘")
-    st.write('result')
+    with st.spinner("시 작성 중"):
+        result = llm.predict(content + "에 대한 시를 써줘")
+        st.write('result')
 
 
 
