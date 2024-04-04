6fccefd945f7bf1a25c87286b6b68947cc8f49f1
88nnn <130357067+88nnn@users.noreply.github.com>
2024-04-04 17:19:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import streamlit as st
-#from langchain_openai.llms import OpenAI
-
 from langchain_openai import ChatOpenAI
-#import os
-
 
 llm = ChatOpenAI()
 
 st.title('시인')
+
 content = st.text_input("엔터로 주제어 입력")
 
 if st.button('시 작성'):
     with st.spinner("시 작성 중"):
-        result = llm.invoke(content + "에 대한 시를 써줘")
+        result = llm.predict(content + "에 대한 시를 써줘")
         st.write(result)
