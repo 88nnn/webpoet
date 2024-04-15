1f0d897697c2470ee2758dea905b91051b5a4dc6
88nnn <130357067+88nnn@users.noreply.github.com>
2024-04-15 21:27:43 +0900

    Update formuse.py

--- formuse.py
+++ formuse.py
@@ -19,17 +19,19 @@ def form_use():
                 user_answer = st.text_input(f"질문 {i + 1}에 대한 답변 입력", "")
                 user_answers.append(user_answer)
 
-            if a == (n-1):
-                if submit_button:
-                    st.write("수고하셨습니다")
+            for a in range(n):
+                if a == (n-1):  # 마지막 반복일 때
+                    if submit_button:
+                        st.write("수고하셨습니다")
+            break  # 반복 종료
+    else:
+        if st.button("풀이"):  # "풀이" 버튼이 클릭되면
+            print(asdf)
+            if st.button("다음 문제"):  # "다음 문제" 버튼을 띄워줌
+                continue  # 다음 반복문으로 넘어감
+        elif st.button("다음 문제"):  # "다음 문제" 버튼이 클릭되면
+            continue  # 다음 반복문으로 넘어감
 
-            if st.button("풀이"):
-                print(asdf)
-                if st.button("다음 문제"):
-                    continue
-
-            if st.button("다음 문제"):
-                continue
 
 if __name__ == "__form_use()__":
     form_use()
